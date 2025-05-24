96e75a2295da394524fd5563d2699ba5db7cc616
Tiwarinjks <shreyashmuni@gmail.com>
2025-05-25 01:25:40 +0530

    name change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          XYZ Folder
+          Shreyash Tiwari
         </p>
         <a
           className="App-link"
