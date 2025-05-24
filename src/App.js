66834ff3d2aa38a14eabb8e7bdc7e2bfc3afde23
Tiwarinjks <shreyashmuni@gmail.com>
2025-05-25 01:17:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,21 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          XYZ Folder
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+            <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          XYZ Folder
         </p>
         <a
           className="App-link"
